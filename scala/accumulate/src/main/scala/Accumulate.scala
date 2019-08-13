3f10467a3b2daf3ec308dcf3ce68d566381c4b15
avinash-anand <avinash.mait@gmail.com>
2019-08-13 08:53:06 +0530

    Accumulate solved

---
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -0,0 +1,20 @@
+import java.util.Objects.nonNull
+
+import scala.annotation.tailrec
+
+class Accumulate {
+  def accumulate[A, B](f: (A) => B, list: List[A]): List[B] = {
+    require(nonNull(list))
+
+    @tailrec
+    def loop(list: List[A], f: (A) => B, acc: List[B]): List[B] = {
+      list match {
+        case Nil => acc
+        case head :: tail => loop(tail, f, acc :+ f(head))
+      }
+    }
+
+    loop(list, f, Nil)
+  }
+
+}
