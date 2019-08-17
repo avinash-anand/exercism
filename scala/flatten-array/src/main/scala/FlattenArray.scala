644d7c60a340b3614908dc4537ac5eb0b2a160b2
avinash-anand <avinash.mait@gmail.com>
2019-08-17 18:18:31 +0530

    another way to flatten

--- scala/flatten-array/src/main/scala/FlattenArray.scala
+++ scala/flatten-array/src/main/scala/FlattenArray.scala
@@ -6,4 +6,15 @@ object FlattenArray {
     case head :: tail => head :: flatten(tail)
   }
 
+  def flatten2(list: List[_]): List[_] = {
+    def innerFlatten(list: List[_], acc: List[_]): List[_] = list match {
+      case Nil => acc
+      case ::(head: List[_], tail) => innerFlatten(tail, innerFlatten(head, acc))
+      case ::(null, tail) => innerFlatten(tail)
+      case ::(head, tail) => innerFlatten(tail, head :: acc)
+    }
+
+    innerFlatten(list, Nil).reverse
+  }
+
 }
