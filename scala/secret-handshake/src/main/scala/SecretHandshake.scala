c36c84250d16d4915202c0c115300386e117a3f7
avinash-anand <avinash.mait@gmail.com>
2019-09-03 09:11:00 +0530

    secret handshake solved

--- scala/secret-handshake/src/main/scala/SecretHandshake.scala
+++ scala/secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,8 +1,29 @@
+import scala.annotation.tailrec
+
 object SecretHandshake {
 
   def commands(i: Int): List[String] = {
-    i.toBinaryString
-    Nil
+    require(i < 32, "Secret handshake not defined for int greater than 31")
+    val binaryRepresentation = i.toBinaryString.toInt
+
+    def getCommands(binaryRep: Int): List[String] = {
+      if (binaryRep >= 10000) {
+        loop(binaryRep - 10000, Nil).reverse
+      } else {
+        loop(binaryRep, Nil)
+      }
+    }
+
+    @tailrec
+    def loop(binaryRep: Int, acc: List[String]): List[String] = {
+      if (binaryRep >= 1000) loop(binaryRep - 1000, "jump" :: acc)
+      else if (binaryRep >= 100) loop(binaryRep - 100, "close your eyes" :: acc)
+      else if (binaryRep >= 10) loop(binaryRep - 10, "double blink" :: acc)
+      else if (binaryRep >= 1) loop(binaryRep - 1, "wink" :: acc)
+      else acc
+    }
+
+    getCommands(binaryRepresentation)
   }
 
 }
