e3f92995040e0407d353315aaf6e9c47d3d98699
avinash-anand <avinash.mait@gmail.com>
2019-09-29 13:25:58 +0530

    java: reverse string solved

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,16 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new StringBuilder(inputString).reverse().toString();
     }
-  
+
+    String reverse2(String inputString) {
+        int length = inputString.length();
+        char[] reversed = new char[length];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[length - 1 - i] = inputString.charAt(i);
+        }
+        return String.valueOf(reversed);
+    }
+
 }
