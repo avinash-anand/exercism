26539eedef82360fcbb20d629a7698d474cf3880
avinash-anand <avinash.mait@gmail.com>
2019-09-29 12:57:36 +0530

    java: isogram solved

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 class IsogramChecker {
 
+    boolean isIsogram2(String phrase) {
+        Set<Character> uniqueCharacters = new HashSet<>();
+        String phraseIgnoreCase = phrase.toLowerCase();
+        return IntStream.range(0, phrase.length())
+                .filter(index -> Character.isAlphabetic(phraseIgnoreCase.charAt(index)))
+                .allMatch(index -> uniqueCharacters.add(phraseIgnoreCase.charAt(index)));
+    }
+
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String phraseIgnoreCase = phrase.replaceAll("[- ]", "").toLowerCase();
+        return phraseIgnoreCase.chars().distinct().count() == phraseIgnoreCase.length();
     }
 
 }
