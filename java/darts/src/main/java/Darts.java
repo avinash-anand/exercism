bd68793d7ae6d64ab30e1f1cb7cb6dd0dc515b11
avinash-anand <avinash.mait@gmail.com>
2019-09-30 09:13:34 +0530

    java: darts solved

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,37 @@
 class Darts {
 
+    private final double x;
+    private final double y;
+    private final double radius;
+
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.x = x;
+        this.y = y;
+        this.radius = calculateRadius();
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (insideInnerCircle()) return 10;
+        if (insideMiddleCircle()) return 5;
+        if (insideOuterCircle()) return 1;
+        return 0;
+    }
+
+    private boolean insideInnerCircle() {
+        return radius <= 1;
+    }
+
+    private boolean insideMiddleCircle() {
+        return radius <= 5;
+    }
+
+    private boolean insideOuterCircle() {
+        return radius <= 10;
+    }
+
+    //hypotenuse = sqrt(sq(base)+sq(height))
+    private double calculateRadius() {
+        return Math.sqrt((x * x) + (y * y));
     }
 
 }
