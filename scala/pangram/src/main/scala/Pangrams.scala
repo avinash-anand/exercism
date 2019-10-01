2988175307e857ed000885af7859958ca5940987
avinash-anand <avinash.mait@gmail.com>
2019-10-01 08:48:29 +0530

    scala: pangrams solved

--- scala/pangram/src/main/scala/Pangrams.scala
+++ scala/pangram/src/main/scala/Pangrams.scala
@@ -1,4 +1,12 @@
 object Pangrams {
-  def isPangram(input: String): Boolean = ???
-}
 
+  private val listOfAlphabets: Seq[Char] = 'a' to 'z'
+
+  def isPangram2(input: String): Boolean = {
+    val lower = input.toLowerCase
+    input.nonEmpty && listOfAlphabets.forall(x => lower.contains(x))
+  }
+
+  def isPangram(input: String): Boolean = input.toLowerCase.filter(_.isLetter).distinct.length == 26
+
+}
