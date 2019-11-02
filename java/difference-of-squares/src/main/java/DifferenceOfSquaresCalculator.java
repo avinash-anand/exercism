65c70b524dc27aaae1e45a9f30aa2a7ebf91eb35
avinash-anand <avinash.mait@gmail.com>
2019-11-02 18:42:51 +0530

    java: difference of squares solved

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,23 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return squareOf(IntStream.rangeClosed(1, input).sum());
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.rangeClosed(1, input)
+                .map(this::squareOf)
+                .sum();
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+    private int squareOf(int number) {
+        return number * number;
     }
 
 }
