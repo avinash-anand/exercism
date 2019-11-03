5e6a27a5839003f27dec4e37dc98e85c8de9414e
avinash-anand <avinash.mait@gmail.com>
2019-11-03 20:52:19 +0530

    java: sum-of-multiples solved

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,35 @@
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 class SumOfMultiples {
 
+    private final int sumOfMultiples;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.sumOfMultiples = Arrays.stream(set)
+                .filter(isNotZero())
+                .flatMap(num -> allMultiplesOfNumLessThan(number, num))
+                .distinct()
+                .sum();
+    }
+
+    private IntStream allMultiplesOfNumLessThan(int number, int num) {
+        return IntStream.rangeClosed(1, number / num)
+                .map(index -> num * index)
+                .filter(isLessThan(number));
+    }
+
+    private IntPredicate isNotZero() {
+        return multiple -> multiple != 0;
+    }
+
+    private IntPredicate isLessThan(int number) {
+        return multiple -> multiple < number;
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return sumOfMultiples;
     }
 
 }
